4583b6277bec6c1eda7cd420724266eefbc2a2a0
nobody <nobody@4a5e1ece-6a47-475c-b646-731d720b41d0>
2003-04-25 19:12:49 +0000

    This commit was manufactured by cvs2svn to create tag 'gfarm_1_0_beta_1'.

---
+++ gfarm/tags/gfarm_1_0_beta_1/lib/libgfarm/gfutil/gfutil.h
@@ -0,0 +1,27 @@
+#ifndef HAVE_DAEMON
+int gfarm_daemon(int, int);
+#else
+#define gfarm_daemon	daemon
+#endif
+
+void gfarm_unlimit_nofiles(int *);
+
+/* logutil */
+
+#ifndef GFARM_DEFAULT_FACILITY
+#define GFARM_DEFAULT_FACILITY	LOG_LOCAL0
+#endif
+
+void gflog_message(int, char *, char *);
+void gflog_error(char *, char *);
+void gflog_warning(char *, char *);
+void gflog_warning_errno(char *);
+
+void gflog_fatal(char *, char *);
+void gflog_fatal_errno(char *);
+
+void gflog_set_identifier(char *);
+void gflog_set_auxiliary_info(char *);
+void gflog_syslog_open(int, int); 
+
+int gflog_syslog_name_to_facility(char *);
