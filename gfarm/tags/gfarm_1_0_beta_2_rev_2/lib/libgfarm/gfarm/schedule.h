71d42a802acfbf9121f40caadc78620a721ff02a
nobody <nobody@4a5e1ece-6a47-475c-b646-731d720b41d0>
2003-05-19 16:59:38 +0000

    This commit was manufactured by cvs2svn to create tag 'gfarm_1_0_beta_2_rev_2'.

---
+++ gfarm/tags/gfarm_1_0_beta_2_rev_2/lib/libgfarm/gfarm/schedule.h
@@ -0,0 +1,7 @@
+/*
+ *
+ */
+
+char *gfarm_schedule_search_idle_hosts(int, char **, int, char **);
+char *gfarm_schedule_search_idle_by_domainname(const char *, int, char **);
+char *gfarm_hosts_schedule_expand_cyclic(int, char ***, int, char **);
