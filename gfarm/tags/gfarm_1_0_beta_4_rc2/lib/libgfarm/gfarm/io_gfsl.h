bc3c0904ad22fe411d345bbdf60a813a8c795e89
nobody <nobody@4a5e1ece-6a47-475c-b646-731d720b41d0>
2003-09-05 15:58:14 +0000

    This commit was manufactured by cvs2svn to create tag 'gfarm_1_0_beta_4_rc2'.

---
+++ gfarm/tags/gfarm_1_0_beta_4_rc2/lib/libgfarm/gfarm/io_gfsl.h
@@ -0,0 +1,13 @@
+struct xxx_connection;
+struct gfarmSecSession;
+struct gfarm_iobuffer;
+
+char *gfarm_gsi_initialize(void);
+
+char *xxx_connection_set_secsession(struct xxx_connection *,
+	struct gfarmSecSession *);
+void xxx_connection_reset_secsession(struct xxx_connection *);
+void gfarm_iobuffer_write_close_secsession_op(struct gfarm_iobuffer *,
+	void *, int);
+
+void xxx_connection_downgrade_to_insecure_session(struct xxx_connection *);
