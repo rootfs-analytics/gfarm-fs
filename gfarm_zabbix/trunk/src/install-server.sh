bb3f55d7ab8d0b9e161248660c901a6d907f459c
m-kasahr <m-kasahr@4a5e1ece-6a47-475c-b646-731d720b41d0>
2012-11-21 08:51:51 +0000

    Add the parameters 'ZABBIX_SERVER_PIDFILE', 'ZABBIX_SERVER_LOGFILE', 'ZABBIX_AGENTD_PIDFILE', 'ZABBIX_AGENTD_LOGFILE', 'GFMD_JOURNALDIR', 'GFARM_DB_PORT', 'GFARM_DB_USER', 'GFARM_DB_PASSWORD' to 'install.conf'; an installation script for 'gfarm_zabbix' (#520).

--- gfarm_zabbix/trunk/src/install-server.sh
+++ gfarm_zabbix/trunk/src/install-server.sh
@@ -9,6 +9,33 @@
 INSTALL=install
 
 #
+# Create "$1" file from "$1.in".
+#
+create_file()
+{
+    [ -f "$1" ] && rm -f "$1"
+    sed -e "s|@ZABBIX_CONFDIR@|$ZABBIX_CONFDIR|g" \
+	-e "s|@SYSLOG_FILE@|$SYSLOG_FILE|g" \
+	-e "s|@ZABBIX_EXTSCRIPTDIR@|$ZABBIX_EXTSCRIPTDIR|g" \
+	-e "s|@ZABBIX_ALERTSCRIPTDIR@|$ZABBIX_ALERTSCRIPTDIR|g" \
+	-e "s|@GFARM_BINDIR@|$GFARM_BINDIR|g" \
+	-e "s|@ZABBIX_DATADIR@|$ZABBIX_DATADIR|g" \
+	-e "s|@APACHE_CONDIR@|$APACHE_CONDIR|g" \
+        -e "s|@ZABBIX_SERVER_PIDFILE@|$ZABBIX_SERVER_PIDFILE|g" \
+        -e "s|@ZABBIX_SERVER_LOGFILE@|$ZABBIX_SERVER_LOGFILE|g" \
+	-e "s|@HTMLDIR@|$HTMLDIR|g" \
+	-e "s|@HTMLDIR_USER@|$HTMLDIR_USER|g" \
+	-e "s|@HTMLDIR_GROUP@|$HTMLDIR_GROUP|g" \
+	-e "s|@PHP_TIMEZONE@|$PHP_TIMEZONE|g" \
+	-e "s|@MYSQL_CONFDIR@|$MYSQL_CONFDIR|g" \
+	-e "s|@DB_NAME@|$DB_NAME|g" \
+	-e "s|@DB_USER@|$DB_USER|g" \
+	-e "s|@DB_PASSWORD@|$DB_PASSWORD|g" \
+	-e "s|@ZABBIX_NODEID@|$ZABBIX_NODEID|g" \
+	"$1.in" > "$1"
+}
+
+#
 # Make a directory '$HTMLDIR'.
 #
 DIR=$HTMLDIR
@@ -55,7 +82,7 @@ fi
 FILE=zabbix_server.conf.sample
 SRCFILE=gfarm-zabbix_confs/zabbix/$FILE
 DSTFILE=$ZABBIX_CONFDIR/$FILE
-create_zabbix_server_file $SRCFILE
+create_file $SRCFILE
 $INSTALL -c -m 0600 -o zabbix -g zabbix $SRCFILE $DSTFILE \
     || { echo "Failed to install the file: $DSTFILE"; exit 1; }
 echo "Install the file: $DSTFILE"
@@ -76,7 +103,7 @@ fi
 FILE=my.cnf.sample
 SRCFILE=gfarm-zabbix_confs/mysql/$FILE
 DSTFILE=$MYSQL_CONFDIR/$FILE
-create_zabbix_server_file $SRCFILE
+create_file $SRCFILE
 if [ "X$MYSQL_CONFDIR" != X ]; then
     $INSTALL -c -m 0644 -o root -g root $SRCFILE $DSTFILE \
 	|| { echo "Failed to install the file: $DSTFILE"; exit 1; }
@@ -99,7 +126,7 @@ fi
 FILE=zabbix.conf.sample
 SRCFILE=gfarm-zabbix_confs/apache/$FILE
 DSTFILE=$APACHE_CONFDIR/$FILE
-create_zabbix_server_file $SRCFILE
+create_file $SRCFILE
 if [ "X$APACHE_CONFDIR" != X ]; then
     $INSTALL -c -m 0644 -o root -g root $SRCFILE $DSTFILE \
 	|| { echo "Failed to install the file: $DSTFILE"; exit 1; }
@@ -119,7 +146,27 @@ for I in \
     Template_Gfarm_redundant_gfsd.xml \
     Template_Gfarm_zabbix.xml; do
     FILE=gfarm-zabbix_templates/$I
-    create_zabbix_server_file $FILE \
+    create_file $FILE \
 	|| { echo "Failed to create the file: $FILE"; exit 1; }
     echo "Create the file: $FILE"
 done
+
+#
+# Create the directory of $ZABBIX_SERVER_PIDFILE.
+#
+DIR=`dirname $ZABBIX_SERVER_PIDFILE`
+if [ ! -d $DIR ]; then
+    $INSTALL -d -m 0755 -o zabbix -g zabbix $DIR \
+	|| { echo "Failed to create the directory: $DIR"; exit 1; }
+    echo "Create the directory: $DIR"
+fi
+
+#
+# Create the directory of $ZABBIX_SERVER_LOGFILE.
+#
+DIR=`dirname $ZABBIX_SERVER_LOGFILE`
+if [ ! -d $DIR ]; then
+    $INSTALL -d -m 0755 -o zabbix -g zabbix $DIR \
+	|| { echo "Failed to create the directory: $DIR"; exit 1; }
+    echo "Create the directory: $DIR"
+fi
