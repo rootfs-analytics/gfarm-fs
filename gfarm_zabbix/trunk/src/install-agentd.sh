01385417979411add668b53269eaf857e6520e01
m-kasahr <m-kasahr@4a5e1ece-6a47-475c-b646-731d720b41d0>
2014-07-07 01:13:39 +0000

    Update the install script of gfarm-zabbix, related to revise of the external scripts (#770).

--- gfarm_zabbix/trunk/src/install-agentd.sh
+++ gfarm_zabbix/trunk/src/install-agentd.sh
@@ -14,6 +14,21 @@ ZABBIX_EXTSCRIPTDIR=$ZABBIX_CONFDIR/externalscripts
 # Directory where configuration files for Zabbix agentd reside.
 ZABBIX_AGENTD_CONFSUBDIR=$ZABBIX_CONFDIR/zabbix_agentd.d
 
+# User name of Zabbix.
+ZABBIX_USER=zabbix
+
+# Group name of Zabbix.
+ZABBIX_GROUP=zabbix
+
+#
+# Get value of a particular parameter defined in 'gfmd.conf'.
+#
+get_gfmd_conf_value()
+{
+    [ -f $GFMD_CONF_FILE ] && \
+        awk '$1 == "'"$1"'" { print $2 }' $GFMD_CONF_FILE
+}
+
 #
 # Create "$1" file from "$1.in".
 #
@@ -23,13 +38,24 @@ create_file()
     [ -f "$1" ] && rm -f "$1"
     sed \
         -e "s|@GFARM_BINDIR@|$GFARM_BINDIR|g" \
-        -e "s|@GFMD_JOURNALDIR@|$GFMD_JOURNALDIR|g" \
-        -e "s|@GFMD_PIDFILE@|$GFMD_PIDFILE|g" \
-        -e "s|@SYSLOG_FILE@|$SYSLOG_FILE|g" \
+        -e "s|@PGHOST@|$PGHOST|g" \
+        -e "s|@PGPORT@|$GPORT|g" \
+        -e "s|@PGDATABASE@|$PGDATABASE|g" \
+        -e "s|@PGUSER@|$PGUSER|g" \
+        -e "s|@PGPASSWORD@|$PGPASSWORD|g" \
         -e "s|@ZABBIX_CONFDIR@|$ZABBIX_CONFDIR|g" \
         -e "s|@ZABBIX_EXTSCRIPTDIR@|$ZABBIX_EXTSCRIPTDIR|g" \
         "$1.in" > "$1"
 }
+
+#
+# Get parameters for accessing PostgreSQL.
+#
+PGHOST="`get_gfmd_conf_value postgresql_server_host`"
+PGPORT="`get_gfmd_conf_value postgresql_server_port`"
+PGDATABASE="`get_gfmd_conf_value postgresql_dbname`"
+PGUSER="`get_gfmd_conf_value postgresql_user`"
+PGPASSWORD="`get_gfmd_conf_value postgresql_password`"
 
 #
 # Install files in 'scripts/zabbix_agentd.d/' to
@@ -62,7 +88,6 @@ fi
 for I in \
     gfarm_generic_client_gfhost.sh \
     gfarm_generic_client_gfmdhost.sh \
-    gfarm_conf.inc \
     gfarm_gfmd_failover.pl \
     gfarm_gfmd_postgresql.sh \
     gfarm_gfsd_gfhost.sh \
@@ -72,10 +97,47 @@ for I in \
     SRCFILE=scripts/externalscripts/$I
     DSTFILE=$ZABBIX_EXTSCRIPTDIR/$I
     create_file $SRCFILE
-    $INSTALL -c -m 0755 -o root -g root $SRCFILE $DSTFILE \
+    $INSTALL -c -m 0755 -o $ZABBIX_USER -g $ZABBIX_GROUP $SRCFILE $DSTFILE \
         || { echo "Failed to install the file: $DSTFILE"; exit 1; }
     echo "Install the file: $DSTFILE"
 done
+
+SRCFILE=scripts/externalscripts/gfarm_conf.inc
+DSTFILE=$ZABBIX_EXTSCRIPTDIR/gfarm_conf.inc
+create_file $SRCFILE
+$INSTALL -c -m 0700 -o $ZABBIX_USER -g $ZABBIX_GROUP $SRCFILE $DSTFILE \
+    || { echo "Failed to install the file: $DSTFILE"; exit 1; }
+echo "Install the file: $DSTFILE"
+
+#
+# Delete or notify obsolete files.
+# 
+OBSOLETE_FILES=
+for I in \
+    zbx_chk_dead_gfarm_pgsql.sh \
+    zbx_chk_dead_gfmd.sh \
+    zbx_chk_dead_gfsd.sh \
+    zbx_chk_gfhost_cli.sh \
+    zbx_chk_gfhost_gfsd.sh \
+    zbx_chk_gfmdhost_cli.sh \
+    zbx_chk_gfmdlist_cli.sh \
+    zbx_chk_gfmdtype_gfmd.sh \
+    zbx_chk_gfsched_gfmd.sh \
+    zbx_chk_gfsched_gfsd.sh \
+    zbx_chk_listenport_gfmd.sh \
+    zbx_chk_listenport_gfsd.sh \
+    zbx_chk_mastername_cli.sh \
+    zbx_chk_pgsql.sh \
+    zbx_failover.pl \
+    zbx_gfarm_utils.inc; do
+    [ -f $ZABBIX_EXTSCRIPTDIR/$I ] && rm -f $ZABBIX_EXTSCRIPTDIR/$I
+done
+
+if [ -f $ZABBIX_EXTSCRIPTDIR/zbx_chk_gfarm.conf ]; then
+    echo "The following file is not used any longer:"
+    echo "    $ZABBIX_EXTSCRIPTDIR/zbx_chk_gfarm.conf"
+    echo
+fi
 
 #
 # Change mode of $SYSLOG_FILE
